89954c663fffd426ac45c014ae92dfb217b22d7f
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-03-27 18:42:01 +0300

    created a component for the button

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
+import Project from "./pages/Project";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
 			{/* <Home /> */}
-			<Projects />
+			{/* <Projects /> */}
+			<Project />
 			{/* <Contacts /> */}
 			<Footer />
 			
