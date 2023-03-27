8b2b982a046e200940c8aa09691ab9743c2d59b3
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-03-27 17:39:10 +0300

    output of all projects using MAP

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import Navbar from "./components/navBar/Navbar";
 
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-
-			<Home />
+			{/* <Home /> */}
+			<Projects />
+			{/* <Contacts /> */}
 			<Footer />
 			
 		</div>
